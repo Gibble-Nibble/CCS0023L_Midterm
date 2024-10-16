340dabe84737f2f14cf749cf74e3215a7e31dc63
Gibble <202311958@fit.edu.ph>
2024-10-16 13:10:17 +0800

    Update Animal.java

--- zoo/Animal.java
+++ zoo/Animal.java
@@ -1,13 +1,11 @@
 public abstract class Animal {
     private String name;
     private int age;
-
+    
     public Animal(String name, int age) {
         this.name = name;
         this.age = age;
     }
-
-    public abstract void talk();
 
     public String getName() {
         return name;
@@ -16,4 +14,6 @@ public abstract class Animal {
     public int getAge() {
         return age;
     }
+
+    public abstract void talk();
 }
