a0bf3c782b82d9f592c135446b3a3953d8b30458
Gibble <202311958@fit.edu.ph>
2024-10-16 12:52:19 +0800

    Update Main.java

--- zoo/Main.java
+++ zoo/Main.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Zoo zoo = new Zoo();
+        Scanner scanner = new Scanner(System.in);
 
-        Lion lion = new Lion("Simba", 5);
-        Tiger tiger = new Tiger("Raja", 3);
+        System.out.print("Enter the type of animal (Lion or Tiger): ");
+        String animalType = scanner.nextLine();
 
-        zoo.addAnimal(lion);
-        zoo.addAnimal(tiger);
+        System.out.print("Enter the animal's name: ");
+        String animalName = scanner.nextLine();
+
+        System.out.print("Enter the animal's age: ");
+        int animalAge = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        Animal animal;
+        if (animalType.equalsIgnoreCase("Lion")) {
+            animal = new Lion(animalName, animalAge);
+        } else if (animalType.equalsIgnoreCase("Tiger")) {
+            animal = new Tiger(animalName, animalAge);
+        } else {
+            System.out.println("Invalid animal type. Exiting.");
+            return;
+        }
+
+        zoo.addAnimal(animal);
 
         zoo.feedingTime();
     }
