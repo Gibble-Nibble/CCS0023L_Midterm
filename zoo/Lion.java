6c2828fff5483c1bfb9578f9ba9a99c08067e9b3
Gibble <202311958@fit.edu.ph>
2024-10-16 13:09:07 +0800

    Update Lion.java

--- zoo/Lion.java
+++ zoo/Lion.java
@@ -2,9 +2,9 @@ public class Lion extends Animal {
     public Lion(String name, int age) {
         super(name, age);
     }
-
+    
     @Override
     public void talk() {
-        System.out.println("I am a Lion named " + getName() + " and I am " + getAge() + " years old.");
+        System.out.println("I am a lion, my name is " + getName() + " and I am " + getAge() + " years old.");
     }
 }
