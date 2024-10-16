d814526f14b611375dd056a9090a07ed26d3b3ec
Gibble <202311958@fit.edu.ph>
2024-10-16 12:45:38 +0800

    Update Main.java

--- zoo/Main.java
+++ zoo/Main.java
@@ -1,1 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
 
+        Lion lion = new Lion("Simba", 5);
+        Tiger tiger = new Tiger("Raja", 3);
+
+        zoo.addAnimal(lion);
+        zoo.addAnimal(tiger);
+
+        zoo.feedingTime();
+    }
+}
