5fa35400a773a7d9aaa013c263bd92a2f019e344
Gibble <202311958@fit.edu.ph>
2024-10-16 12:47:19 +0800

    Update Zoo.java

--- zoo/Zoo.java
+++ zoo/Zoo.java
@@ -1,1 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Zoo {
+    private List<Animal> animals;
+
+    public Zoo() {
+        animals = new ArrayList<>();
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+    public void removeAnimal(Animal animal) {
+        animals.remove(animal);
+    }
+
+    public void feedingTime() {
+        for (Animal animal : animals) {
+            animal.talk();
+        }
+    }
+}
