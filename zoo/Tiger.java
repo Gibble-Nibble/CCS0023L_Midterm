e6f1ddcb16b123fbaaeb8e0f10a66d9fca9172b0
Gibble <202311958@fit.edu.ph>
2024-10-16 12:47:37 +0800

    Update Tiger.java

--- zoo/Tiger.java
+++ zoo/Tiger.java
@@ -1,1 +1,10 @@
+public class Tiger extends Animal {
+    public Tiger(String name, int age) {
+        super(name, age);
+    }
 
+    @Override
+    public void talk() {
+        System.out.println("I am a Tiger named " + getName() + " and I am " + getAge() + " years old.");
+    }
+}
